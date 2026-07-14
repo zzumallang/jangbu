2347c8d34a2186301747406ec14d00fc75c2d13a
zzumallang <ghsnf@naver.com>
2026-07-14 10:46:52 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "jangbu-v1";
+const CACHE = "jangbu-v2";
 const FILES = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
